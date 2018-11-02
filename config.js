409d07c03968ec5d4cb20f39c18439157d76a69f
Hozefa Mangalorewala <hozefam@gmail.com>
2018-11-02 18:46:46 +0300

    Initial Code

---
+++ config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  ENV: process.env.NODE_ENV || 'development',
+  PORT: process.env.PORT || 3000,
+  URL: process.env.BASE_URL || 'http://localhost:3000/',
+  MONGODB_URI:
+    process.env.MONGODB_URI ||
+    'mongodb://abc123!:abc123!@ds131384.mlab.com:31384/customer_api'
+};
